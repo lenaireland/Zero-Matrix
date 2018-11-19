42105259bdae77cc3ac13fdd594c7c36f396d7ea
Hackbright Student <no-reply@hackbrightacademy.com>
2018-11-19 10:54:35 -0800

    Finished coding challenge

---
+++ zeromatrix.py
@@ -0,0 +1,44 @@
+
+"""Given an NxM matrix, if a cell is zero, set entire row and column to zeroes.
+
+A matrix without zeroes doesn't change:
+
+    >>> zero_matrix([[1, 2 ,3], [4, 5, 6], [7, 8, 9]])
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+But if there's a zero, zero both that row and column:
+
+    >>> zero_matrix([[1, 0, 3], [4, 5, 6], [7, 8, 9]])
+    [[0, 0, 0], [4, 0, 6], [7, 0, 9]]
+
+Make sure it works with non-square matrices:
+
+    >>> zero_matrix([[1, 0, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+    [[0, 0, 0, 0], [5, 0, 7, 8], [9, 0, 11, 12]]
+"""
+
+
+def zero_matrix(matrix):
+    """Given an NxM matrix, for cells=0, set their row and column to zeroes."""
+
+    zeros = []
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 0:
+                zeros.append([i,j])
+
+    for zero in zeros:
+        for i in range(len(matrix)):
+            matrix[i][zero[1]] = 0
+
+        for j in range(len(matrix[zero[0]])):
+            matrix[zero[0]][j] = 0
+
+    return matrix
+
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** TESTS PASSED! YOU'RE DOING GREAT!\n")
